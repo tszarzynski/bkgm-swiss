124f9f6742dbf3e75ca7a7b33a0424405e4bb001
Tomasz Szarzynski <tszarzynski@gmail.com>
2019-11-29 17:33:49 +0000

    Changed exports

--- build/index.js
+++ build/index.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var pair_1 = require("./pair");
 exports.pairPlayers = pair_1.pairPlayers;
-var round_1 = require("./round");
-exports.playRound = round_1.playRound;
 var utils_1 = require("./utils");
 exports.roundsNeeded = utils_1.roundsNeeded;
+var rank_1 = require("./rank");
+exports.rankPlayers = rank_1.rankPlayers;
+exports.calcOMV = rank_1.calcOMV;
 //# sourceMappingURL=index.js.map
